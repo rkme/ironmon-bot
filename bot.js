972c2f06d30bc3da60390d7a1a5bdbe7f6a3b910
Rohan Mehta <rohanmeh@andrew.cmu.edu>
2020-12-17 17:10:57 +0000

    Added Flask server to handle https requests from UptimeRobot . Now stays active 24/7, attempts to reconnect if disconnected. Added support for multi word queries.

--- bot.js
+++ bot.js
@@ -1,18 +1,37 @@
 const tmi = require('tmi.js');
 const bst = require('./bst.json');
+const express = require('express')
 
-// Cause glitch to ping the bot itself to stay awake
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
+// Using this script to keep_alive.py in node
+const app = express()
+let runPy = new Promise(function(success, nosuccess) {
+
+    const { spawn } = require('child_process');
+    const pyprog = spawn('python', ['./keep_alive.py']);
+
+    pyprog.stdout.on('data', function(data) {
+
+        success(data);
+    });
+
+    pyprog.stderr.on('data', (data) => {
+
+        nosuccess(data);
+    });
 });
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+
+app.get('/', (req, res) => {
+
+    res.write('welcome\n');
+
+    runPy.then(function(fromRunpy) {
+        console.log(fromRunpy.toString());
+        res.end(fromRunpy);
+    });
+})
+
+var p = 8080; //unsure if this should be a different port
+app.listen(p, () => console.log(`Application listening on port ${p}!`))
 
 // Define configuration options
 const opts = {
@@ -22,7 +41,11 @@ const opts = {
   },
   channels: [
     process.env.CHANNEL_NAME
-  ]
+  ],
+  connection: {
+    reconnect: true
+  }
+  
 };
 
 // Create a client with our options
@@ -41,22 +64,38 @@ function onMessageHandler (target, context, msg, self) {
 
   // Remove whitespace from chat message
   const commandName = msg.split(' ')[0];
+  // const keyword = msg.split(' ').slice(1).join(" ")  // Deprecated static entry
 
   // If the command is known, let's execute it
-  if (commandName === '!bst') {
-    var keyword = msg.split(' ')[1]
-    if (keyword.toLowerCase() in bst) {
-      client.say(target, `${keyword} BST: ${bst[keyword]}`);
-    } else {client.say(target, "No pokemon with that name found.")}
+  if (commandName.toLowerCase() === '!bst') {
+    const keyword = findKeyword(msg, bst)
+    if (keyword && keyword.toLowerCase() in bst) {
+      console.log(`${context["username"]}: ${msg} (found ${bst[keyword]})`)
+      var name = keyword.charAt(0).toUpperCase() + keyword.slice(1); // Capitalize name
+      client.say(target, `${name} BST: ${bst[keyword]}`);
+    } else {client.say(target, "No pokemon with that name found.")
+            console.log(`Failed BST search: ${msg}`)}
   } else if (commandName === '!terry') {
     client.say(target, 'RIP Terry :(');
   }
 }
 
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 20;
-  return Math.floor(Math.random() * sides) + 1;
+// Finds keyword to a command by comparing to dict keys. Also finds multiword keywords
+// as long as the full name directly follows the commandName
+function findKeyword(message, dict) {
+    const words = message.split(' ')  // Splits up all words in message 
+    var i;
+    for (i = 1; i <= words.length; i++){
+        var kw = words.slice(1,i).join(" ").toLowerCase();
+        if (kw in dict){
+          // console.log(`Found keyword: ${kw}`);
+          return kw;
+        }
+        else {continue}
+    }
+    // Only gets here if it message contains a valid command but not a valid keyword // directly after it.
+    console.log(`No keyword found in ${message}.`)
+    return false; // returns False if no keyword provided
 }
 
 // Called every time the bot connects to Twitch chat
